1afd9d4f86b51ced2ec4aebabe891d34e264f0bf
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-04-30 22:06:39 -0400

    added google font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,10 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
