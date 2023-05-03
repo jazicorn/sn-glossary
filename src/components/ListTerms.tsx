f5d80141e4555e0f9b8b75e365d82d3c7c9c6d50
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-03 13:41:36 -0400

    updated window responsiveness

--- src/components/ListTerms.tsx
+++ src/components/ListTerms.tsx
@@ -41,15 +41,20 @@ export default function ListOfTerms() {
     }
 
     return (
-        <div className='md:h-fit md:w-5/6 md:my-5 md:mr-5 md:ml-2 bg-slate-700 rounded-lg border border-slate-400
-                        w-auto h-4/5 mx-5 mb-5 mt-1 pb-1'>
+        <div className='md:w-5/6 md:my-5 md:mr-5 md:ml-2 
+                        bg-slate-700 rounded-lg border border-slate-400
+                        w-auto h-fit mx-5 mb-5 mt-1 pb-1'>
+            
             <h2 className="text-xl m-2 bg-blue-100 border-4 border-slate-100 rounded">
                 &#123;List&#125;: Terms
             </h2>
             {/** #TODO Map form to display multiple term examples */}
             <form className='flex flex-col'>
                 <>
+                    {/**All Term Information*/}
                     <div className='flex flex-col mx-2 my-1 p-1 bg-gray-200 rounded'>
+
+                        {/**Line: Term Definition */}
                         <div className='flex flex-row bg-gray-100 rounded m-1 p-1'>
                             <label className='m-1 h-6 basis-16 grow-0 shrink-0 text-base bg-pink-100 rounded'>
                                 Term:
@@ -65,7 +70,10 @@ export default function ListOfTerms() {
                             />
                         </div>
                         
-                        <div className='flex flex-row rounded p-1 items-baseline'>
+                        {/**Lines: Refs & Tags*/}
+                        <div className='md:flex md:flex-row flex flex-col rounded p-1 items-baseline'>
+
+                            {/**Ref*/}
                             <div className='flex flex-row mr-1 bg-gray-100 rounded'>
                                 <label className='m-1 mx-2 px-2 basis-14 text-sm
                                                 bg-blue-300 rounded border-2 border-slate-400'>
@@ -86,7 +94,9 @@ export default function ListOfTerms() {
 
                                 </div>
                             </div>
-                            <div className='flex flex-row bg-gray-100 rounded'>
+
+                            {/**Tags*/}
+                            <div className='flex flex-row bg-gray-100 rounded mt-1'>
                                 <label className='m-1 mx-2 px-2 basis-14 text-sm bg-blue-300 rounded border-2 border-slate-400'>Tags:</label>
                                 <div className='m-1 text-left flex flex-row text-xs'>
                                     {getTerm.tags.map((tag, index) => (
