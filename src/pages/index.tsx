5bcd9e8e5723fcead6a7bba6aa15e2d22d114bce
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-06 17:28:00 -0400

    updated mailing list modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,40 +3,43 @@ import { Inter } from 'next/font/google';
 import Link from 'next/link';
 // Initialization for ES Users
 import {
+  Input,
   Modal,
   Ripple,
   initTE,
 } from "tw-elements";
 
-initTE({ Modal, Ripple });
+initTE({ Input, Modal, Ripple });
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div className={`flex h-screen min-h-full flex-col ${inter.className}`}>
-      {/*****************/}
-      {/**<!-- Modal -->*/}
-      {/*****************/}
+      {/**************************/}
+      {/**<!-- mailList Modal -->*/}
+      {/**************************/}
       <div
         data-te-modal-init
         className="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
         id="mailListModal"
         tab-index="-1"
-        aria-labelledby="exampleModalLabel"
+        aria-labelledby="mailListModalLabel"
         aria-hidden="true">
         <div
           data-te-modal-dialog-ref
           className="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+          {/**popup background */}
           <div
-            className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+            className="bg-gradient-to-b from-grayish to-gray-100 min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
             <div
-              className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+              className="flex flex-shrink-0 items-center justify-between rounded-t-md p-4 dark:border-opacity-50">
               {/**<!--Modal title-->*/}
               <h5
                 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
-                id="exampleModalLabel">
-                Modal title
+                id="mailListModalLabel">
+                <span className='text-base font-semibold'>SN-Glossary: </span>
+                <span className='text-base font-light'>Mailing List</span>
               </h5>
               {/**<!--Close button-->*/}
               <button
@@ -60,27 +63,30 @@ export default function Home() {
             </div>
 
             {/**<!--Modal body-->*/}
-            <div className="relative flex-auto p-4" data-te-modal-body-ref>
-              Modal body text goes here.
+            {/**<div className="relative flex-auto p-4 italic" data-te-modal-body-ref>
+              search
+            </div>*/}
+            <div className="relative m-3" data-te-input-wrapper-init>
+              <input
+                type="text"
+                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="exampleFormControlInput1"
+                placeholder="" />
+              <label
+                htmlFor="exampleFormControlInput1"
+                className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary italic"
+                >email
+              </label>
             </div>
-
             {/**<!--Modal footer-->*/}
             <div
-              className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+              className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md p-4 ">
               <button
                 type="button"
-                className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-                data-te-modal-dismiss
+                className="ml-1 inline-block rounded bg-primary px-4 py-1.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                 data-te-ripple-init
                 data-te-ripple-color="light">
-                Close
-              </button>
-              <button
-                type="button"
-                className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                data-te-ripple-init
-                data-te-ripple-color="light">
-                Save changes
+                Subscribe
               </button>
             </div>
           </div>
