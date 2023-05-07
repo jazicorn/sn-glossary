683cf2cd5394313fc68146da3f2b9be09b4441ba
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-07 03:52:12 -0400

    added tauri toolkit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { Inter } from 'next/font/google';
 import dynamic from "next/dynamic";
+import { invoke } from '@tauri-apps/api/tauri'
+import { useEffect } from 'react';
+
 const NavBar = dynamic(import("../components/NavBarHome"), { ssr: false });
 const NavFeature = dynamic(import("../components/NavBarFeature"), { ssr: false });
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  useEffect(() => {
+    invoke('greet', { name: 'World' })
+    .then(console.log)
+    .catch(console.error)
+  }, []);
   return (
     <div className={`flex h-screen min-h-full flex-col ${inter.className}`}>
       {/**Navbar component */}
