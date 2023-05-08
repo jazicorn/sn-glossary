bbf1089b734407db0140fe87ed856b9a211ab2ef
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-08 01:01:09 -0400

    added border to feature buttons

--- src/components/NavBarFeature.tsx
+++ src/components/NavBarFeature.tsx
@@ -20,24 +20,32 @@ export default function Home() {
                 <div className='place-self-center justify-items-center text-center grid gap-3 grid-row-2 grid-cols-3 md:grid-row-1 md:grid-cols-4 md:gap-1'>
                     <div
                         className='custom-feature-bar-tabs text-sm col-start-2 col-end-2 md:col-start-1 md:col-end-1'>
-                        <button onClick={() => setShowComponent(<Search/>)} className="">
+                        <button
+                            onClick={() => setShowComponent(<Search />)}
+                            className="px-1 border border-transparent rounded focus:border-blue-200">
                             Word Search
                         </button>
                         <p className='italic text-xs text-yellow-500'>*In Progress*</p>
                     </div>
                     <div className='custom-feature-bar-tabs text-sm col-start-1 md:col-start-2'>
-                        <button onClick={() => setShowComponent(<CustomList/>)} className="">
+                        <button
+                            onClick={() => setShowComponent(<CustomList />)}
+                            className="px-1 border border-transparent rounded focus:border-blue-200">
                             Customized List
                         </button>
                         <p className='italic text-xs text-rose-400'>*Coming Soon*</p>
                     </div>
                     <div className='custom-feature-bar-tabs text-sm col-start-2 md:col-start-3'>
-                        <button onClick={() => setShowComponent(<FlashCards/>)} className="">
+                        <button
+                            onClick={() => setShowComponent(<FlashCards />)}
+                            className="px-1 border border-transparent rounded focus:border-blue-200">
                             Flashcards
                         </button>
                         <p className='italic text-xs text-rose-400'>*Coming Soon*</p></div>
                     <div className='custom-feature-bar-tabs text-sm col-start-3 md:col-start-4'>
-                        <button onClick={() => setShowComponent(<Desktop/>)} className="">
+                        <button
+                            onClick={() => setShowComponent(<Desktop />)}
+                            className="px-1 border border-transparent rounded focus:border-blue-200">
                             Desktop Support
                         </button>
                         <p className='italic text-xs text-rose-400'>*Coming Soon*</p>
