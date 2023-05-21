58582613e4171ba3a839b68b7ee48e02ec9920a6
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-21 19:49:03 -0400

    changed search focus color

--- src/components/feature/FeatureSearch.tsx
+++ src/components/feature/FeatureSearch.tsx
@@ -17,7 +17,7 @@ export default function NavBarFeatureSearch() {
               type='search'
               id='word-search'
               placeholder='search'
-              className='bg-grayish-100 relative m-0 min-w-0 basis-96 rounded border-2 border-solid border-neutral-300 bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-neutral-400 focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary'
+              className='bg-grayish-100 relative m-0 min-w-0 basis-96 rounded border-2 border-solid border-neutral-300 bg-clip-padding px-3 py-[0.25rem] font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-neutral-400 focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary focus:border-sn-dark'
             ></input>
           </div>
         </div>
