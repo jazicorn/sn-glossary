d69fd5a46338ab3e17ff73f46e8834e8dc7e1e1b
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-22 15:03:58 -0400

    added redirect for example page

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/example',
+        has: [
+          {
+            type: 'header',
+            key: 'host',
+            value: 'sn-glossary.com',
+          },
+        ],
+        destination: '/',
+        permanent: false,
+      },
+    ]
   },
 };
 
