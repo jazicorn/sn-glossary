f939df67d1a0c2da05c6f66196582a12fcaec1bf
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-24 10:59:03 -0400

    added email support

---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { render } from "@react-email/render";
+import WelcomeTemplate from "../../../emails/WelcomeTemplate";
+import { sendEmail } from "../../../lib/email";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // extract email from request
+  const { body } = req;
+
+  // complete if POST method 
+  if (req.method === 'POST') {
+    // try to send email using given email address
+    try {
+      await sendEmail({
+        to: body.email,
+        subject: "Welcome to NextAPI",
+        html: render(WelcomeTemplate()),
+      });
+
+      return res.status(200).json({ message: "Email sent successfully" });
+      
+    } catch(e) {
+      res.status(400).send(e);
+    }
+  } else {
+    res.status(405).send({
+        message: `The HTTP ${req.method} method is not supported at this route.`,
+    });
+  }
+}
