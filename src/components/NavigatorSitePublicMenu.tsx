67c56cc3af2a78e7398e65e5ee00cc3ef3ae8229
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-24 00:10:17 -0400

    removed blue background from menu

--- src/components/NavigatorSitePublicMenu.tsx
+++ src/components/NavigatorSitePublicMenu.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function NavigatorSitePublicMenu() { 
     return (
-        <div className="bg-blue-100 py-4 my-4 mx-24 flex flex-col place-items-center text-slate-700 text-center border rounded-lg">
+        <div className="py-4 my-4 mx-24 flex flex-col place-items-center text-slate-700 text-center border rounded-lg">
             <h1 className="text-2xl w-full border-b border-slate-300 pb-2">Links</h1>
             <div className="divide-y-2">
                 <ul className="">
