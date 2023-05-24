987eb3ea73482bd0126b3c9aeab3a7187f7e79d7
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-24 13:24:44 -0400

    added info to feature page

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -1,30 +1,30 @@
 import { Inter } from 'next/font/google';
 // import React, { Dispatch, ReactNode, SetStateAction, createContext, useContext, useState } from "react"
 import dynamic from 'next/dynamic';
+import Footer from '../components/Footer'
 const NavBar = dynamic(import('../components/NavigatorSitePublic'), { ssr: false });
-import FeatureList from '@/components/feature/FeatureList';
+const CallToAction = dynamic(import('../components/CallToAction'), { ssr: false });
+const NavFeature = dynamic(import('../components/feature/NavigatorFeature'), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ['latin'] });
 
 // Terms Public Directory
 export default function Features() {
-  return (
-    <div
-      className={`flex h-screen min-h-screen w-screen flex-col bg-slate-100 xl:grow ${inter.className} bg-no-repeat `}
-    >
+ return (
+    <div className={`flex h-screen min-h-full flex-col ${inter.className}`}>
       {/**Navbar component */}
       <NavBar />
-      {/**<!-- content goes here --> */}
-      <div className='flex grow flex-row bg-white bg-gradient-to-r from-gray-100 to-blue-100 text-center'>
-        <div className='flex grow flex-col rounded-xl xl:flex xl:flex-row'>
-          {/** React Component that List all Terms from selected list */}
-          {/**Replace {List} title with list name */}
-          <div className='pb-1 my-5 ml-2 mr-5 h-auto w-auto grow rounded-xl border bg-transparent '>
-            {/******************* */}
-            <FeatureList />
-          </div>
-        </div>
-      </div>
+
+      {/**CalltoAction component */}
+      <CallToAction/>
+      
+      {/**Feature component*/}
+      <NavFeature />
+
+      {/**Footer */}
+      <Footer />
     </div>
   );
 }
