a903feaa12852c0e2c1f48c8d5907087b91f4441
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-26 23:09:32 -0400

    added favorite icon to public glossary

--- src/components/glossary/PublicTerm.tsx
+++ src/components/glossary/PublicTerm.tsx
@@ -5,30 +5,43 @@ import {
   faChevronCircleUp,
 } from '@fortawesome/free-solid-svg-icons';
 import { GlossaryContextType } from '../../../lib/types';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 
 const PublicTerm = ({ term }: { term: GlossaryContextType }) => {
   const [showMore, setShowMore] = useState(false);
   function handleMoreClick() {
     setShowMore(!showMore);
   }
+
+  const [favorite, setFavorite] = useState(false);
+  function getfavorite() {
+    setFavorite(!favorite);
+  }
+
   return (
-    <div key='term' className=''>
-      <div
-        key='undefined'
-        className='mx-2 my-1 flex flex-row justify-between rounded bg-gray-200 p-1'
-      >
-        {/**Line: Term Definition */}
-        <div
-          key='undefined'
-          className='mb-1 ml-1 mr-2 mt-1 flex grow flex-row justify-between rounded bg-gray-100 p-1 '
-        >
-          <div
-            key='undefined'
-            className='content-left flex grow flex-row rounded-l border border-r-0 border-gray-300 text-sm leading-tight tracking-tighter'
-          >
+    <div>
+      <div className='mx-2 my-1 flex flex-row justify-between rounded bg-gray-200 p-1'>
+        
+        <div className='mb-1 ml-1 mr-2 mt-1 flex grow flex-row justify-between rounded bg-gray-100 p-1'>
+          {/**Star Favorite*/}
+          <button key='term-index' onClick={getfavorite} className='w-fit px-2 flex flex-col justify-center'>
+            {!favorite ? <FontAwesomeIcon
+              icon={faStar}
+              size='lg'
+              color={'#334155'}
+            /> : <FontAwesomeIcon
+              icon={faStarSolid}
+              size='lg'
+              color={'#334155'}
+            />
+            }
+          </button>
+          {/**Line: Term Definition */}
+          <div className='content-left flex grow flex-row rounded-l border border-l-0 border-r-0 border-gray-300 text-sm leading-tight tracking-tighter'>
             <div
               key={term.name}
-              className='w-24 rounded-l border-2 border-blue-300 bg-slate-200 p-0.5 text-center focus:border-2 focus:border-blue-400 focus:outline-none'
+              className='w-24 rounded border-2 border-blue-300 bg-slate-200 p-0.5 text-center focus:border-2 focus:border-blue-400 focus:outline-none'
             >
               {term.name}
             </div>
@@ -63,118 +76,66 @@ const PublicTerm = ({ term }: { term: GlossaryContextType }) => {
         </div>
       </div>
       {/**More Info */}
-      {/**More Info */}
-      {/**More Info */}
       {showMore && (
-        <div
-          key='termshowmore'
-          className='custom-term-details mx-2 flex flex-col rounded bg-gray-200 p-1  xl:flex xl:flex-row'
-        >
-          {/**Lines: Refs & Tags*/}
-          <div
-            key='undefined'
-            className='mt-1 flex flex-wrap items-baseline rounded px-1 xl:m-0  xl:flex xl:flex-row xl:flex-nowrap'
-          >
+        <div className='custom-term-details mx-2 flex flex-col rounded bg-gray-200 p-1'>
+          {/**Lines: Details*/}
+          <div className='mt-1 flex flex-col items-baseline rounded px-1 xl:m-0 xl:flex xl:flex-row'>
             {/**ID*/}
-            <div
-              key='undefined'
-              className='flex w-full flex-row rounded bg-gray-100 sm:mr-1 sm:w-auto sm:w-fit'
-            >
-              <label
-                key='undefined'
-                className='mb-1 ml-1 mt-1 rounded-l border-2 border-blue-300 bg-slate-400 pl-2 pr-1 text-sm'
-              >
+            <div className='w-full xl:w-fit mr-1 flex flex-row rounded bg-gray-100 xl:w-auto xl:grow'>
+              <label className='flex-none w-[90px] mb-1 ml-1 mt-1 pl-2 pr-1 rounded-l border-2 border-blue-300 bg-slate-200 text-sm text-right'>
                 ID:
               </label>
-              <div
-                key='undefined'
-                className='custom-input-id mr-1 mt-1 inline w-full sm:w-fit'
-              >
+              <div className='w-full xl:w-[90px] flex flex-row mb-1 mr-1 mt-1 inline text-left text-xs'>
                 <div
-                  key={term.id}
+                  key='id'
                   placeholder='123456789'
-                  className='h-6 w-full grow rounded-r border border-gray-300 bg-transparent pl-1 pt-0.5 text-left text-sm focus:border-slate-400 focus:outline-none sm:w-[88px]'
-                >
-                  {term.id}
-                </div>
-              </div>
-            </div>
-            {/**Ref*/}
-            <div
-              key='undefined'
-              className='mt-1 flex w-full flex-row rounded bg-gray-100 sm:mr-1 sm:w-min xl:mt-0 xl:w-full'
-            >
-              <label
-                key='undefined'
-                className='mb-1 ml-1 mt-1 basis-[92px] rounded-l border-2 border-slate-400 bg-violet-100 pl-2 pr-1 text-sm'
-              >
-                Ref:
-              </label>
-              <label
-                key='undefined'
-                className='mb-1 mt-1 basis-14 border border-r-0 border-gray-300 bg-transparent pl-1 pr-0.5 pt-1 text-right align-bottom text-xs'
-              >
-                https://
-              </label>
-              <div
-                key='undefined'
-                className='sm:no-grow mr-1 mt-1 inline w-auto grow text-left text-xs'
-              >
-                <div
-                  key={term.ref}
-                  className='h-6 w-auto min-w-[12rem] overflow-hidden text-ellipsis rounded-r border border-gray-300 bg-transparent pl-1 pt-1 text-left text-xs focus:border-slate-400 focus:outline-none'
-                >
-                  <a href={'https:' + term.ref} target='_blank'>
-                    ServiceNow: Product Doc URL
-                  </a>
-                </div>
+                  className='h-6 w-full xl:w-[90px] justify-center pt-[3px] rounded-r border border-gray-300 bg-transparent p-0 pl-1 text-left text-xs focus:border-slate-400 focus:outline-none'>{term.id}</div>
+                
               </div>
             </div>
             {/**Category */}
-            <div
-              key='undefined'
-              className='mt-1 flex w-full flex-row rounded bg-gray-100 sm:w-fit xl:mr-1 xl:mt-0'
-            >
-              <label
-                key='undefined'
-                className='mb-1 ml-1 mt-1 basis-[92px] rounded-l border-2 border-slate-400 bg-blue-300 pl-2 pr-1  text-sm '
-              >
-                Category:
+            <div className='mr-1 mt-1 w-full xl:w-max flex flex-row no-wrap rounded bg-gray-100 xl:mt-0'>
+              <label className='flex-none mb-1 ml-1 mt-1 pl-2 pr-1 w-[90px] rounded-l border-2 border-slate-300 bg-violet-100 text-sm text-right'>
+                Product:
               </label>
-              <div
-                key='undefined'
-                className='mr-1 mt-1 inline w-max grow text-left text-xs xl:w-fit'
-              >
+              <div className='mr-1 mt-1 w-full xl:fit inline text-left text-xs'>
+                {/**For wrapping text have to use textarea */}
                 <div
-                  key={term.product}
-                  placeholder='(Product) Category'
-                  className='h-6 w-full rounded-r border border-gray-300 bg-transparent pl-1 pt-0.5 text-left text-sm focus:border-slate-400 focus:outline-none sm:w-[10rem]'
-                >
-                  {term.product}
-                </div>
+                  placeholder='Product'
+                  className='px-1 pt-1 h-6 w-full xl:fit truncate rounded-r border-l-0 border border-gray-300 bg-transparent text-left text-xs focus:border-slate-400 focus:outline-none'
+                >{ term.product }</div>
               </div>
             </div>
+            {/**Ref*/}
+            <div className='mr-1 xl:m-0 mt-1 w-full flex flex-row rounded bg-gray-100 xl:mt-0 grow'>
+              <label className='flex-none mb-1 ml-1 mt-1 pl-2 pr-1 w-[90px] rounded-l border-2 border-slate-300 bg-violet-100 text-sm text-right'>
+                Ref:
+              </label>
+              <div className='mr-1 mt-1 w-full inline text-left text-xs'>
+                <button
+                  onClick={() => window.open('https://' + `${term.ref}`, '_blank' )}
+                  placeholder='https://docs.servicenow.com/'
+                  className='pl-1 h-6 min-w-[225px] w-full rounded-r border-l-0 border border-gray-300 bg-transparent text-left text-xs focus:border-slate-400 focus:outline-none'
+                >ServiceNow: Product Documentation</button>
+              </div>
+            </div>
+          </div>
+          {/**Lines:Tags*/}
+          <div className='w-full px-1 pb-1'>
             {/**Tags*/}
-            <div
-              key='undefined'
-              className='mt-1 flex w-fit flex-row rounded bg-gray-100 xl:mr-1 xl:mt-0 '
-            >
-              <label
-                key='undefined'
-                className='mb-1 ml-1 mt-1 basis-14 rounded border-2 border-slate-400 bg-blue-300 pl-2 pr-1 text-sm '
-              >
+            <div className='mt-1 w-full flex flex-row flex-wrap rounded bg-gray-100 rounded-r border border-transparent focus:border-slate-400 focus:outline-none'>
+              <label className='flex-none max-h-6 w-[90px] mb-1 ml-1 mt-1 pr-1 rounded-l border-2 border-slate-300 bg-slate-100 text-sm text-right '>
                 Tags:
               </label>
-              <div
-                key='tags'
-                className='mr-1 flex flex-row flex-wrap content-center items-center sm:flex-nowrap'
-              >
-                {term?.tags?.map((tag) => (
+              
+              {/**Input-tags */}
+              <div className='mr-1 p-1 flex flex-row flex-wrap content-center items-center text-xs'>
+                {term.tags?.map((tag) => (
                   <div
                     key='tag'
-                    className='m-1 flex h-6 w-auto flex-row content-center items-center rounded border-0 bg-slate-200 pr-2 text-sm focus:border-2 focus:border-slate-400'
+                    className='mr-1 mb-1 h-6 w-fit pr-2 text-sm flex flex-row content-center items-center rounded border-0 bg-slate-200 focus:border-2 focus:border-slate-400'
                   >
-                    <span className='w-auto truncate pb-0.5 pl-1 text-left'>
+                    <span className='w-fit pb-0.5 pl-1 text-right'>
                       {tag}
                     </span>
                   </div>
