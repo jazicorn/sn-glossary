2fdbbd732d735ccb7b3e97659462691fdbc3d4ab
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-29 07:59:01 -0400

    removed unused tauri imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { Inter } from 'next/font/google';
 import dynamic from 'next/dynamic';
-import { invoke } from '@tauri-apps/api/tauri';
-import { useEffect } from 'react';
 import Footer from '../components/Footer';
 import LandingPage from '@/components/LandingPage';
 const NavBar = dynamic(import('../components/navigator/NavigatorSitePublic'), {
@@ -11,9 +9,7 @@ const NavBar = dynamic(import('../components/navigator/NavigatorSitePublic'), {
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  useEffect(() => {
-    invoke('greet', { name: 'World' }).then(console.log).catch(console.error);
-  }, []);
+
   return (
     <div
       className={`relative z-0 flex h-screen min-h-full flex-col justify-between ${inter.className}`}
