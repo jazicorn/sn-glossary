3c22f3d81a9b9886d3738e73c036a1191f1422db
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-05-30 22:59:33 -0400

    fixed list title

--- src/components/dashboard/ListTerms.tsx
+++ src/components/dashboard/ListTerms.tsx
@@ -17,7 +17,7 @@ export default function ListOfTerms() {
   return (
     <div className='mx-5 mb-5 mt-1 h-fit w-auto rounded-xl border border-slate-400 bg-slate-700 pb-1 xl:my-5 xl:ml-2 xl:mr-5 xl:h-auto xl:grow'>
       <h2 className='custom-list-term-nav m-2 rounded border-4 border-slate-100 bg-blue-100 text-xl text-deep-blue'>
-        <FontAwesomeIcon icon={faStar} /><span className='px-2'>{menuItem}</span>
+        {menuItem === "Favorites" || "Public Glossary" ? <div><FontAwesomeIcon icon={faStar} /><span className='px-2'>{menuItem}</span></div> : <div>{menuItem}</div>}
       </h2>
       <TermSearch />
       <hr className='my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-300 to-transparent opacity-50 dark:opacity-100' />
