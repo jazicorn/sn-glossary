c70edd56c285154cc2151a30189543efdcd7335a
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-06-09 14:14:06 -0400

    removed unused file

--- src/context/contextUser.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-import { UserContextType } from '../../lib/types';
-
-// api call for user object using session data
-//export const userObj: UserContextType
-// creat context for user object
-export const UserContext = createContext<UserContextType | null>(null);
