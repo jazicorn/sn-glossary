73e993ffd60b19f52644fa03a61fe95dc1b59391
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-06-09 14:04:46 -0400

    connected public glossary to pouchdb

--- src/components/glossary/PublicGlossary.tsx
+++ src/components/glossary/PublicGlossary.tsx
@@ -1,46 +1,65 @@
-import React, { useContext } from 'react';
-import { GlossaryContext } from '../../context/contextGlossary';
+import React from 'react';
+// import { GlossaryContext } from '../../context/contextGlossary';
 import Term from './PublicTerm';
 import Search from './PublicSearch';
+import { dbPublic } from '@/context/contextGlossary';
+import { useCallback, useEffect, useState } from 'react'
+import { GlossaryContextType } from '../../../lib/types';
 
-export default function Glossary() {
-  const publicGlossary = useContext(GlossaryContext);
+export default function PublicGlossary() {
+  //const publicGlossary = useContext(GlossaryContext);
+  const [data, setData] = useState<GlossaryContextType[]>([]);
+
+  // declare the async data fetching function
+  const fetchData = useCallback(async () => {
+    const docs = await dbPublic.allDocs({ include_docs: true });
+    //console.log(docs);
+    const docsJSON = JSON.stringify(docs.rows);
+    //console.log(docsJSON);
+    if (docsJSON != undefined) {
+      const parsed = JSON.parse(docsJSON);
+      //console.log(parsed);
+      // let counter = 0
+      const result:GlossaryContextType[] = [];
+      //console.log(parsed[0].doc);
+      parsed.forEach((element: { doc: GlossaryContextType; }) => {
+       //console.log(element.doc)
+       result.push(element.doc)
+      });
+      if (result != undefined) {
+        setData(result);
+      }
+    }
+  }, [])
+
+  // the useEffect is only there to call `fetchData` at the right time
+  useEffect(() => {
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);;
+  }, [fetchData])
 
   return (
     <>
       {/**Page Title */}
-      <h1
-        key='undefined'
-        className='text-bold m-2 my-4 rounded border bg-gray-100 py-1 text-xl text-sn-dark xl:text-2xl'
-      >
-        ServiceNow{' '}
-        <span key='undefined' className='text-md text-sn-light xl:text-lg'>
-          Glossary
-        </span>
+      <h1 className='text-bold m-2 my-4 rounded border bg-gray-100 py-1 text-xl text-sn-dark xl:text-2xl'>
+        ServiceNow <span className='text-md text-sn-light xl:text-lg'> Glossary </span>
       </h1>
-      <hr
-        key='undefined'
-        className='my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-300 to-transparent opacity-50 dark:opacity-100'
+      <hr className='my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-300 to-transparent opacity-50 dark:opacity-100'
       />
-      <div key='undefined' className='flex flex-col '>
+      <div key='search' className='flex flex-col '>
         {/******************* */}
         {/****Search Bar***** */}
         {/******************* */}
         <Search />
       </div>
-      <hr
-        key='undefined'
-        className='my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-300 to-transparent opacity-50 dark:opacity-100'
-      />
-
+      <hr className='my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-300 to-transparent opacity-50 dark:opacity-100'/>
       <>
         {/******************* */}
         {/**Display All Terms */}
         {/******************* */}
-        {publicGlossary?.map((term) => (
-          <div key='term'>
-            <Term {...{ term }} />
-          </div>
+        {data?.map((term) => (
+          <Term key={term.id} term={term} />
         ))}
       </>
     </>
--- src/context/contextGlossary.tsx
+++ src/context/contextGlossary.tsx
@@ -1,6 +1,31 @@
-import { createContext } from "react";
+import React, { createContext } from "react";
+import PouchDB from 'pouchdb'
 import publicGlossary from '../../utils/dataExPublicGlossary';
 import { GlossaryContextType } from '../../lib/types';
+
+const publicData:GlossaryContextType[] = publicGlossary
+/**Create Public GLossary Database */
+export const dbPublic = new PouchDB('glossaryPublic');
+/**<!-- Seed Public Glossary Database-> */
+const seedDBPublic = async (data: GlossaryContextType[]) => {
+  data.forEach((item) => { item._id = item.id });
+  try {
+    //console.log(data)
+    await dbPublic.bulkDocs(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+seedDBPublic(publicData);
+
+export const getPublicDoc = async (id: string) => { 
+  try {
+    const result = await dbPublic.get<Document>(id)
+    return JSON.stringify(result);
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 //https://refine.dev/blog/usecontext-and-react-context/
 
@@ -9,11 +34,33 @@ export const GlossaryContext = createContext<GlossaryContextType[] | null>(
   publicGlossary
 );
 
+// const defaultValues: GlossaryData = {
+//   glossary: publicData,
+//   getGlossaryTerm: 'Favorites',
+//   addGlossaryTerm: false,
+//   deleteGlossaryTerm: '',
+// };
+
+
+// const myGlossary= {
+//     state: defaultValues,
+//     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+//     dispatch: (action: GlossaryAction) => {},
+// };
+  
+
 interface Props {
   children: React.ReactNode;
 }
 
+// //export const DashboardContext = createContext(myData);
+// export const GlossaryContext = createContext<{
+//   state: GlossaryData;
+//   dispatch: React.Dispatch<GlossaryAction>;
+// }>(myGlossary); //initialize context with default value
+
 export const GlossaryProvider: React.FC<Props> = ({ children }) => {
+  // const [state, dispatch] = useReducer(GlossaryReducer, defaultValues);
   
   return (
     <GlossaryContext.Provider value={publicGlossary}>
