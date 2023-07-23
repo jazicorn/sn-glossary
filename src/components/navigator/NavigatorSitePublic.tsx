4aa4d9964c51320cf6e324f8e0582482f0dd2892
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-07-23 13:59:49 -0400

    fixed: mobile menu exits off target

--- src/components/navigator/NavigatorSitePublic.tsx
+++ src/components/navigator/NavigatorSitePublic.tsx
@@ -12,9 +12,14 @@ import {
   faBook,
   faGifts,
   faMinus,
+  faStar,
+  faFileLines,
+  faLightbulb,
+  faMapLocationDot,
+  faHandshakeAngle,
 } from '@fortawesome/free-solid-svg-icons';
+
 import { faSun,faCircleUser as faUser } from '@fortawesome/free-regular-svg-icons';
-import NavigatorSitePublicMenu from './NavigatorSitePublicMenu';
 import AccountMenuLogout from './menu/AccountMenuLogout';
 import AccountMenuLogin from './menu/AccountMenuLogin';
 import MoreMenu from './menu/MoreMenu';
@@ -297,9 +302,152 @@ export default function NavigatorSitePublic() {
           </div>
         </nav>
       </div>
-      {/**DropDown Menu */}
-      <div ref={wrapperRef} className='z-1000 absolute flex w-screen flex-col items-center overflow-visible border-b-2 bg-white sm:items-start  sm:border-0 sm:bg-transparent xl:hidden'>
-        {Menu && <NavigatorSitePublicMenu />}
+      {/**Mobile DropDown Menu */}
+      <div  className='z-1000 absolute flex w-screen flex-col items-center overflow-visible border-b-2 bg-white sm:items-start  sm:border-0 sm:bg-transparent xl:hidden'>
+        {Menu &&
+         <div ref={wrapperRef} className='mx-8 my-4 flex flex-col place-items-center rounded-lg border bg-gray-100 py-4 text-center text-slate-700'>
+          <h1 className='w-full border-b border-slate-300 px-10 pb-2 text-xl'>
+            {' '}
+            Navigation
+          </h1>
+          <div className='divide-y-2 '>
+            <ul>
+              <ol className='flex flex-row justify-center p-2'>
+                {/**<!-- Glossary--> */}
+                <div className='no-wrap flex flex-row'>
+                  <button className='mx-1 flex flex-row text-blue-500'>
+                    <Link
+                      href='/glossary'
+                      className='text-grey-900 hover:text-grey-300 flex h-fit w-auto flex-row place-self-center truncate px-0.5 py-0 text-center tracking-tighter'
+                    >
+                      <FontAwesomeIcon
+                        icon={faBook}
+                        size='sm'
+                        className='place-self-center'
+                      />
+                      <span className='pl-1 tracking-tighter'>
+                        Public Glossary
+                      </span>
+                    </Link>
+                  </button>
+                </div>
+              </ol>
+              <ol className='flex flex-row justify-center p-2'>
+                <button className='mx-1 flex flex-row'>
+                  <Link
+                    href='/features'
+                    className='text-grey-900 hover:text-grey-300 h-fit w-auto place-self-center truncate px-0.5 py-0 text-center tracking-tighter'
+                  >
+                    <FontAwesomeIcon icon={faStar} size='sm' />
+                    <span className='px-0.5'>Features</span>
+                  </Link>
+                </button>
+              </ol>
+              <ol className='flex flex-row justify-center p-2'>
+                <button className='mx-1 flex flex-row'>
+                  <Link
+                    href=''
+                    className='text-grey-900 hover:text-grey-300 h-fit w-auto place-self-center truncate px-0.5 py-0 text-center'
+                  >
+                    <FontAwesomeIcon icon={faMapLocationDot} size='sm' />
+                    <span className='px-0.5'>Roadmap</span>
+                  </Link>
+                </button>
+              </ol>
+              
+              <ol className='flex flex-row justify-center p-2'>
+                <button className='mx-1 flex flex-row'>
+                  <Link
+                    href=''
+                    className='text-grey-900 hover:text-grey-300 h-fit w-auto place-self-center truncate px-0.5 py-0 text-center tracking-tighter'
+                  >
+                    <FontAwesomeIcon icon={faLightbulb} size='sm' className='w-4'/>
+                    F.A.Q
+                  </Link>
+                </button>
+              </ol>
+              <ol className='flex flex-row justify-center p-2'>
+                <button className='mx-1 flex flex-row'>
+                  <Link
+                    href=''
+                    className='text-grey-900 hover:text-grey-300 h-fit w-auto place-self-center truncate px-0.5 py-0 text-center tracking-tighter'
+                  >
+                    <FontAwesomeIcon icon={faFileLines} size='sm' className='w-4'/>Documentation
+                  </Link>
+                </button>
+              </ol>
+              <ol className='flex flex-row justify-center p-2'>
+                <button className='mx-1 flex flex-row'>
+                  <Link
+                    href=''
+                    className='text-grey-900 hover:text-grey-300 h-fit w-auto place-self-center truncate px-0.5 py-0 text-center tracking-tighter'
+                  >
+                    <FontAwesomeIcon icon={faHandshakeAngle} size='sm' />
+                    <span className='px-0.5'>Helpful Links</span>
+                  </Link>
+                </button>
+              </ol>
+              <ol className='flex flex-row justify-center p-2'>
+                <button className='mx-1 flex flex-row'>
+                  <Link
+                    href=''
+                    className='text-grey-900 hover:text-grey-300 flex h-fit w-auto flex-row place-self-center truncate px-0.5 py-0 text-center tracking-tighter'
+                  >
+                    <FontAwesomeIcon
+                      icon={faChalkboard}
+                      size='sm'
+                      className='place-self-center'
+                    />
+                    <Link href='/dashboard' className='px-1'>Dashboard</Link>
+                    <FontAwesomeIcon
+                      icon={faArrowUpRightFromSquare}
+                      color=''
+                      size='2xs'
+                    />
+                  </Link>
+                </button>
+              </ol>
+            </ul>
+            <ul>
+              <ol className='flex flex-row justify-center p-2'>
+                {/**<!-- ServiceNow External Link --> */}
+                <button className='mx-1 flex flex-row '>
+                  <a
+                    href='https://docs.servicenow.com/'
+                    target='_blank'
+                    className='text-grey-900 hover:text-grey-300 h-fit w-auto place-self-center truncate px-0.5 py-0 text-center text-sn-light'
+                  >
+                    ServiceNow
+                  </a>
+                  <FontAwesomeIcon
+                    icon={faArrowUpRightFromSquare}
+                    color=''
+                    size='2xs'
+                  />
+                </button>
+              </ol>
+              <ol className='flex flex-row justify-center p-2'>
+                <button className='mx-1 flex flex-row'>
+                  <Link
+                    href=''
+                    className='text-grey-900 hover:text-grey-300 h-fit w-auto place-self-center truncate px-0.5 py-0 text-center tracking-tighter'
+                  >
+                    Developer
+                  </Link>
+                  <FontAwesomeIcon
+                    icon={faArrowUpRightFromSquare}
+                    color=''
+                    size='2xs'
+                  />
+                </button>
+              </ol>
+            </ul>
+            <ul>
+              <ol className='flex flex-row justify-center p-2'>...</ol>
+            </ul>
+          </div>
+        </div>
+        }
       </div>
       <div ref={wrapperRef} className='z-1000 absolute flex w-screen flex-col items-center overflow-visible border-b-2 bg-white sm:items-end  sm:border-0 sm:bg-transparent'>
         {accountMenu && isSignedIn && <AccountMenuLogout />}
